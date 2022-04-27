064a5b54e40f0c4a7f7267a1914b86a53f4b25fd
wpsantos16 <gutocaioinduscar@outlook.com>
2022-04-27 18:03:10 -0300

    projeto versao final

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# covid19_info_dio
-Projeto desenvolvido no bootcamp desenvolvimento web da carrefour em parceria com a DIO.
+versão final
---
+++ src/containers/Main/components/Board.jsx
@@ -0,0 +1,35 @@
+import React, { memo } from 'react'
+import PropTypes from 'prop-types'
+import { Grid, Skeleton } from '../../../components'
+import Card from './Card'
+
+function Board({ data }) {
+  const { cases, todayDeaths, recovered, deaths, todayCases, totalTests } = data
+
+  const getValue = (value) => value ? value : <Skeleton variant="text" width={182} height={60} />
+
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(cases)} label="Total de casos" color="#5d78ff" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(todayDeaths)} label="Óbitos hoje" color="#E95078" /> 
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(todayCases)} label="Casos hoje" color="#F7B829" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(deaths)} label="Total de mortos" color="#000" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(recovered)} label="Total de recuperados" color="#67C887" />
+      </Grid>
+      <Grid item xs={12} md={3}>
+        <Card value={getValue(totalTests)} label="Total de testes" color="#c0c0c0" />
+      </Grid>
+    </Grid>
+  )
+}
+
+export default memo(Board)
